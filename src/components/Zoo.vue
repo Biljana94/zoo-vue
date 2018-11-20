28b8dd0cb84d38ec8b58faba1d416723b87bf73a
Biljana Jelaca <bilja.jelaca@gmail.com>
2018-11-20 19:29:22 +0100

    Move animal to top of the list

--- src/components/Zoo.vue
+++ src/components/Zoo.vue
@@ -9,17 +9,25 @@
                     <th scope="col">Name</th>
                     <th scope="col">Date of Birth</th>
                     <th scope="col">Remove Animal</th>
+                    <th scope="col">Move to top</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="(animal, index) in animals" :key="index"> <!--pomocu for petlje prolazimo kroz niz i ispisujemo u tabeli zivotinje-->
                     <td>{{animal.species}}</td>
                     <td>{{animal.name}}</td>
+
                     <!--Ako zivotinja nema datum rodjenja napisati da je datum nepoznat-->
                     <td>{{animal.dateOfBirth ? animal.dateOfBirth : 'Unknown'}}</td>
+
                     <td>
                         <!--na klik dugmeta se pokrece akcija removeAnimal(animal), koja brise zivotinju, ta metoda se nalazi u export default-->
                         <button @click="removeAnimal(animal)">Remove</button>
+                    </td>
+
+                    <td>
+                        <!--Datu zivotinju da mozemo na klik dugmeta da pomerimo na vrh liste-->
+                        <button @click="moveToTop(animal)">Move to top</button>
                     </td>
                 </tr>
             </tbody>
@@ -53,6 +61,12 @@ export default {
         removeAnimal(animal) {
             let index = this.animals.indexOf(animal); //u let index stavljamo index svake zivotinje
             this.animals.splice(index, 1); //splice - brisemo sa index pozicije 1 element;
+        },
+
+        //
+        moveToTop(animal) {
+            this.removeAnimal(animal); //iskoristili smo metodu removeAnimal() koja nam sklanja neku zivotinju, a posto nam je ne brise bez fnc splice() onda smo je pomerili u sledecoj liniji na pocetak niza
+            this.animals.unshift(animal);//i nad nizom zivotinja smo pozvali fnc unshift() koja nam pomera zivotinju na prvo mesto u listi
         }
     }
     
