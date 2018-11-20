69f4d843c611ea8c632e253ad7978901261c4f41
Biljana Jelaca <bilja.jelaca@gmail.com>
2018-11-20 19:08:14 +0100

    Add unknown dateOfBirth of animal

--- src/components/Zoo.vue
+++ src/components/Zoo.vue
@@ -1,6 +1,7 @@
 <template>
 
     <div class="container">
+        <h2>Animals</h2>
         <table class="table">
             <thead>
                 <tr>
@@ -13,7 +14,8 @@
                 <tr v-for="(animal, index) in animals" :key="index"> <!--pomocu for petlje prolazimo kroz niz i ispisujemo u tabeli zivotinje-->
                     <td>{{animal.species}}</td>
                     <td>{{animal.name}}</td>
-                    <td>{{animal.dateOfBirth}}</td>
+                    <!--Ako zivotinja nema datum rodjenja napisati da je datum nepoznat-->
+                    <td>{{animal.dateOfBirth == '' ? 'Unknown' : animal.dateOfBirth}}</td>
                 </tr>
             </tbody>
         </table>
@@ -23,6 +25,7 @@
 
 
 <script>
+
 export default {
     //data fnc vraca neki objekat
     data() {
@@ -33,7 +36,8 @@ export default {
                 {species: 'macka', name: 'Mitar', dateOfBirth: '15.9.2018'},
                 {species: 'slon', name: 'Dambo', dateOfBirth: '5.3.2017'},
                 {species: 'riba', name: 'Nemo', dateOfBirth: '11.11.2011'},
-                {species: 'zaba', name: 'Sima', dateOfBirth: '1.2.2001'}
+                {species: 'zaba', name: 'Sima', dateOfBirth: '1.2.2001'},
+                {species: 'zmija', name: 'Sladja', dateOfBirth: ''}
             ]
         }
     },
